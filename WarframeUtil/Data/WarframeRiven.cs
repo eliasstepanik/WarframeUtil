9de2449c2c22ce0655ba2848c8730a0b4b5ab834
nnuuvv <jonas.streun@outlook.com>
2022-01-13 00:11:42 +0100

    Popular rolls detailview|UnrolledAvg|removed datapoints object

--- WarframeUtil/Data/WarframeRiven.cs
+++ WarframeUtil/Data/WarframeRiven.cs
@@ -10,4 +10,39 @@ public class Riven
     public int UnfilteredPriceMin { get; set; }
     public int UnfilteredPriceMax { get; set; }
     public double UnfilteredPriceAvg { get; set; }
+    public RivenStats RivenStats { get; set; }
+    public double AverageUnrolled { get; set; }
+}
+
+public class RivenStats
+{
+    public RivenStats()
+    {
+        Buffs = new List<Buff>();
+        Curses = new List<Curse>();
+    }
+    public List<Buff> Buffs { get; set; }
+    public List<Curse> Curses { get; set; }
+}
+
+public class Buff
+{
+    public Buff(string name, int count)
+    {
+        Name = name;
+        Count = count;
+    }
+    public string Name { get; set; }
+    public int Count { get; set; }
+}
+
+public class Curse
+{
+    public Curse(string name, int count)
+    {
+        Name = name;
+        Count = count;
+    }
+    public string Name { get; set; }
+    public int Count { get; set; }
 }
